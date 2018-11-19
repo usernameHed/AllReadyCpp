9997226ad4780d856ad7ad9879e2181fdf02a867
DESKTOP-20HJOKH\Hed <ugo@belfiore.fr>
2018-11-19 11:22:43 +0100

    Ok Ready

---
+++ AllReadyCpp/AllReadyCpp/Drill.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "Drill.h"
+
+
+Drill::Drill()
+{
+
+}
+
+
+Drill::~Drill()
+{
+
+}
+
+void Drill::Print(std::string message)
+{
+	std::cout << "toto" << std::endl;
+}
---
+++ AllReadyCpp/AllReadyCpp/Drill.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Drill
+{
+public:
+	Drill();
+	~Drill();
+
+	void Print(std::string message);
+};
+
